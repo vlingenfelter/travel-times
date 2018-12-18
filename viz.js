381a09a52fa03e2c29e68ca1219b684651eff0b0
Violet Lingenfelter <vlingenfelter5@gmail.com>
2018-12-18 11:38:51 -0500

    made histogram svg resize automatically

--- viz.js
+++ viz.js
@@ -1,3 +1,6 @@
+
+// makeHistogram: Array of travel times, line color -> Histogram svg
+// renders the histogram of travel times as well as the legend
 function makeHistogram(values, color) {
 
   // A formatter for counts.
@@ -16,14 +19,13 @@ function makeHistogram(values, color) {
       bottom: 30,
       left: 30
     },
-    // this tells us the width for use when creating the histogram bars
-    width = parseInt(d3.select('#histogram').style('width'), 10);
-    width = histogramWidth - margin.left - margin.right;
-    height = width * 0.7 - margin.top - margin.bottom;
+  // this tells us the width for use when creating the histogram bars
+  width = parseInt(d3.select('#histogram').style('width'), 10);
+  //  width = histogramWidth - margin.left - margin.right;
+  height = width * 0.6 - margin.top - margin.bottom;
 
-    legendWidth = parseInt(d3.select('#legend').style('width'), 10);
+  legendWidth = parseInt(d3.select('#legend').style('width'), 10);
 
-  //legendWidth = svgWidth * .3;
 
   // Calculate stats
   max = d3.max(values);
@@ -33,12 +35,12 @@ function makeHistogram(values, color) {
   // this is how many times were recorded
   numberOfTimes = values.length;
   // sort the times to find the percentiles
-  sortedTimes = values.sort((a,b) => (a - b));
+  sortedTimes = values.sort((a, b) => (a - b));
   // get 15th, 20th, and 25th percentile for use in deciding
   // new benchmark travel times
-  var per15coeff =  Math.floor(numberOfTimes*.15) - 1;
-  var per20coeff =  Math.floor(numberOfTimes*.2) - 1;
-  var per25coeff =  Math.floor(numberOfTimes*.25) - 1;
+  var per15coeff = Math.floor(numberOfTimes * .15) - 1;
+  var per20coeff = Math.floor(numberOfTimes * .2) - 1;
+  var per25coeff = Math.floor(numberOfTimes * .25) - 1;
   perc15 = sortedTimes[per15coeff];
   perc20 = sortedTimes[per20coeff];
   perc25 = sortedTimes[per25coeff];
@@ -77,6 +79,7 @@ function makeHistogram(values, color) {
     .attr("preserveAspectRatio", "xMinYMin meet")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .attr("class", "histogram")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -112,94 +115,47 @@ function makeHistogram(values, color) {
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
-  legend = d3.select("#legend").append("svg")
-    .attr("x", histogramWidth + margin.right)
-    .attr("width", legendWidth + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  legend = d3.select("#legend").append("div")
+    .style("height", height + margin.top + margin.bottom)
+    .attr("class", "legend")
+    .style("margin", "1em")
+    .style("padding", "1em")
+    .style("border", "10px solid #1fa898")
+    .style("text-align", "center");
 
-  legend.append("rect")
-    .attr("x", 1)
-    .attr("width", legendWidth - margin.left - margin.right)
-    .attr("height", height - margin.top - margin.bottom)
-    .attr("fill", "#fff")
-    .attr("stroke", "#1fa898")
-    .attr("stroke-width", 10);
 
-  legend.append("text")
-    .attr("x", ((legendWidth - margin.left - margin.right) / 2))
-    .attr("y", margin.top)
-    .attr("text-anchor", "middle")
+  legend.append("h3")
     .style("font-size", "2em")
     .style("font-family", 'Comfortaa')
     .style("text-decoration", "underline")
     .text("Summary");
 
-  legend.append("text")
+  legend.append("p")
     .attr("class", "info perc15")
-    .attr("x", margin.left)
-    .attr("y", (margin.top + 40))
-    .style("font-size", "1.5em")
-    .style("font-family", 'Comfortaa')
     .text("15th Percentile: " + String(perc15.toFixed(2)) + " min");
 
-  legend.append("text")
+  legend.append("p")
     .attr("class", "info perc20")
-    .attr("x", margin.left)
-    .attr("y", (margin.top + 70))
-    .style("font-size", "1.5em")
-    .style("font-family", 'Comfortaa')
     .text("20th Percentile: " + String(perc20.toFixed(2)) + " min");
 
-  legend.append("text")
+  legend.append("p")
     .attr("class", "info perc25")
-    .attr("x", margin.left)
-    .attr("y", (margin.top + 100))
-    .style("font-size", "1.5em")
-    .style("font-family", 'Comfortaa')
     .text("25th Percentile: " + String(perc25.toFixed(2)) + " min");
 
-  legend.append("text")
+  legend.append("p")
     .attr("class", "info med")
-    .attr("x", margin.left)
-    .attr("y", (margin.top + 150))
-    .style("font-size", "1.5em")
-    .style("font-family", 'Comfortaa')
     .text("Median: " + String(med.toFixed(2)) + " min");
 
-  legend.append("text")
+  legend.append("p")
     .attr("class", "info num")
-    .attr("x", margin.left)
-    .attr("y", (margin.top + 200))
-    .style("font-size", "1.5em")
-    .style("font-family", 'Comfortaa')
     .text("Entries: " + String(numberOfTimes));
-
-  /*
-  legend.append("text")
-    .attr("class", "info from")
-    .attr("x", margin.left)
-    .attr("y", (margin.top + 250))
-    .style("font-size", "1.5em")
-    .style("font-family", 'Comfortaa')
-    .text("From: " + fromDateString);
-
-  legend.append("text")
-    .attr("class", "info to")
-    .attr("x", margin.left)
-    .attr("y", (margin.top + 280))
-    .style("font-size", "1.5em")
-    .style("font-family", 'Comfortaa')
-    .text("To: " + toDateString);
-    */
 }
 
 function getDataTable() {
   tableValues = [];
 
   // populate table json array with histogram data
-  for (var i=0; i < data.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     var max = parseFloat(d3.max(data[i])).toFixed(2);
     var min = parseFloat(d3.min(data[i])).toFixed(2);
     // check to see if min and max are the same value
@@ -210,52 +166,62 @@ function getDataTable() {
         var bin = min + " to " + max;
       }
       var count = data[i].length;
-      tableValues.push({minutes: bin, count: count});
+      tableValues.push({
+        Minutes: bin,
+        Count: count
+      });
     }
   }
 
   // put this data into a data table
   function tabulate(data, columns) {
-		var table = d3.select('#dataTableContainer')
-                .append('table')
-                .attr("id", "datatable")
-                .style("display", "none")
-		var thead = table.append('thead')
-		var	tbody = table.append('tbody');
+    var table = d3.select('#dataTableContainer')
+      .append('table')
+      .attr("id", "datatable")
+      .style("display", "none")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody');
 
-		// append the header row
+    // append the header row
     table.append('caption')
-          .text('Number of trips per histogram bin');
+      .text('Number of trips per histogram bin');
 
-		thead.append('tr')
-		  .selectAll('th')
-		  .data(columns).enter()
-		  .append('th')
-        .attr("scope", "col")
-		    .text(function (column) { return column; });
+    thead.append('tr')
+      .selectAll('th')
+      .data(columns).enter()
+      .append('th')
+      .attr("scope", "col")
+      .text(function(column) {
+        return column;
+      });
 
-		// create a row for each object in the data
-		var rows = tbody.selectAll('tr')
-		  .data(data)
-		  .enter()
-		  .append('tr')
+    // create a row for each object in the data
+    var rows = tbody.selectAll('tr')
+      .data(data)
+      .enter()
+      .append('tr')
       .attr('scope', 'row');
 
-		// create a cell in each row for each column
-		var cells = rows.selectAll('td')
-		  .data(function (row) {
-		    return columns.map(function (column) {
-		      return {column: column, value: row[column]};
-		    });
-		  })
-		  .enter()
-		  .append('td')
-		    .text(function (d) { return d.value; });
+    // create a cell in each row for each column
+    var cells = rows.selectAll('td')
+      .data(function(row) {
+        return columns.map(function(column) {
+          return {
+            column: column,
+            value: row[column]
+          };
+        });
+      })
+      .enter()
+      .append('td')
+      .text(function(d) {
+        return d.value;
+      });
 
-	  return table;
-	}
+    return table;
+  }
 
-  tabulate(tableValues, ['minutes', 'count']);
+  tabulate(tableValues, ['Minutes', 'Count']);
   var displayButton = d3.select("#dataTableButton").style("display", "block")
 }
 
@@ -267,7 +233,7 @@ function refreshDataTable() {
 }
 
 function displayDataTable() {
-   d3.select("#datatable").style("display", "table")
+  d3.select("#datatable").style("display", "table")
 }
 
 /*
@@ -296,10 +262,10 @@ function refresh(values) {
   med = d3.median(values);
   mean = d3.mean(values);
   numberOfTimes = values.length;
-  sortedTimes = values.sort((a,b) => (a - b));
-  var per15coeff =  Math.floor(numberOfTimes*.15) - 1;
-  var per20coeff =  Math.floor(numberOfTimes*.2) - 1;
-  var per25coeff =  Math.floor(numberOfTimes*.25) - 1;
+  sortedTimes = values.sort((a, b) => (a - b));
+  var per15coeff = Math.floor(numberOfTimes * .15) - 1;
+  var per20coeff = Math.floor(numberOfTimes * .2) - 1;
+  var per25coeff = Math.floor(numberOfTimes * .25) - 1;
   perc15 = sortedTimes[per15coeff];
   perc20 = sortedTimes[per20coeff];
   perc25 = sortedTimes[per25coeff];
@@ -320,7 +286,7 @@ function refresh(values) {
     .call(xAxis);
 
 
-  legend.select("text.info.perc15")
+  legend.select("p.info.perc15")
     .transition()
     .duration(500)
     .style("opacity", 0)
@@ -328,7 +294,7 @@ function refresh(values) {
     .style("opacity", 1)
     .text("15th Percentile: " + String(perc15.toFixed(2)) + " min");
 
-  legend.select("text.info.perc20")
+  legend.select("p.info.perc20")
     .transition()
     .duration(500)
     .style("opacity", 0)
@@ -336,7 +302,7 @@ function refresh(values) {
     .style("opacity", 1)
     .text("20th Percentile: " + String(perc20.toFixed(2)) + " min");
 
-  legend.select("text.info.perc25")
+  legend.select("p.info.perc25")
     .transition()
     .duration(500)
     .style("opacity", 0)
@@ -344,7 +310,7 @@ function refresh(values) {
     .style("opacity", 1)
     .text("25th Percentile: " + String(perc25.toFixed(2)) + " min");
 
-  legend.select("text.info.med")
+  legend.select("p.info.med")
     .transition()
     .duration(500)
     .style("opacity", 0)
@@ -352,34 +318,13 @@ function refresh(values) {
     .style("opacity", 1)
     .text("Median: " + String(med.toFixed(2)) + " min");
 
-  legend.select("text.info.num")
+  legend.select("p.info.num")
     .transition()
     .duration(500)
     .style("opacity", 0)
     .transition().duration(500)
     .style("opacity", 1)
     .text("Number of Entries: " + String(numberOfTimes));
-
-    /*
-  legend.select("text.info.from")
-    .transition()
-    .duration(500)
-    .style("opacity", 0)
-    .transition().duration(500)
-    .style("opacity", 1)
-    .text("From: " + fromDateString);
-
-
-  legend.select("text.info.to")
-    .transition()
-    .duration(500)
-    .style("opacity", 0)
-    .transition().duration(500)
-    .style("opacity", 1)
-    .text("To: " + toDateString);
-  */
-
-
 
   bar = svg.selectAll(".bar").data(data);
 
@@ -422,4 +367,90 @@ function buttonColorTransition() {
   d3.selectAll(".color-change").transition()
     .duration(500)
     .style("background-color", color);
+}
+
+d3.select(window).on('resize', resize);
+
+function resize() {
+
+  values = times;
+  // update width
+  width = parseInt(d3.select('#histogram').style('width'));
+  width = width - margin.left - margin.right;
+
+  legendWidth = parseInt(d3.select('#legend').style('width'), 10);
+
+  data = d3.layout.histogram()
+    .bins(x.ticks(20))
+    (values);
+
+  // Reset y domain using new data
+  yMax = d3.max(data, function(d) {
+    return d.length
+  });
+  yMin = d3.min(data, function(d) {
+    return d.length
+  });
+  y.domain([0, yMax]);
+
+  d3.select("#histogram").select("svg.histogram")
+    .transition()
+    .duration(100)
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+
+
+  x = d3.scale.linear()
+    .domain([min, max])
+    .range([0, width]);
+
+
+  xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat((d) => (d + " min"));
+
+  svg.select("g.x.axis")
+    .transition()
+    .duration(100)
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+
+  bar = svg.selectAll(".bar").data(data);
+
+  // Remove object with data
+  bar.exit().remove();
+
+
+  bar.transition()
+    .duration(100)
+    .attr("transform", function(d) {
+      return "translate(" + x(d.x) + "," + y(d.y) + ")";
+    });
+
+  bar.select("rect")
+    .transition()
+    .duration(100)
+    .attr("x", 1)
+    .attr("width", (x(data[0].dx) - x(0)) - 1)
+    .attr("height", function(d) {
+      return height - y(d.y);
+    })
+    .attr("fill", function(d) {
+      return colorScale(d.y)
+    });
+
+  bar.select("text")
+    .transition()
+    .duration(100)
+    .attr("dy", ".75em")
+    .attr("y", -12)
+    .attr("x", (x(data[0].dx) - x(0)) / 2)
+    .attr("text-anchor", "middle")
+    .text(function(d) {
+      return formatCount(d.y);
+    });
+
 }
